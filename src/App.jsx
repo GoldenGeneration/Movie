194085699ab282ff09b47f60ef676c5a3ac8cda5
Bishwo <115875934+GoldenGeneration@users.noreply.github.com>
2026-03-03 19:07:32 +0545

    landing page and TMDB fetch api tested

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from "react";
+import Search from "./components/search";
+import Spinner from "./components/Spinner";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMsg, setErrMsg] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrMsg("");
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
+      }
+
+      const data = await response.json();
+
+      if (data.response === "False") {
+        setErrMsg(data.Error || "Failed to fetch movies");
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+    } catch (error) {
+      console.log(`Error fetching movies: ${error}`);
+      setErrMsg("Error Fetching Movies. Please try again later");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find your <span className="text-gradient">Movies</span> you Enjoy
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+        <section className="all-movies">
+          <h2 className="mt-20">Trending movies</h2>
+          {isLoading ? (
+            <p>
+              <Spinner />
+            </p>
+          ) : errorMsg ? (
+            <p className="text-red-500">{errorMsg} </p>
+          ) : (
+            <ul>
+              {" "}
+              {movieList.map((movie) => (
+                <p key={movie.id} className="text-white">
+                  {movie.title}
+                </p>
+              ))}
+            </ul>
+          )}
+        </section>
+      </div>
+    </main>
+  );
+}
+
+// const App = () => {
+//     return (
+//       <div>
+
+//           <h1>Hey</h1>
+//       </div>
+//   );
+// };
+
+export default App;
---
+++ src/components/search.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Search({ searchTerm, setSearchTerm }) {
+  return (
+      <div className="search">
+          <div>
+              
+      <img src="search.svg" alt="search" />
+
+      <input
+        type="text"
+        placeholder="Search your choice"
+        value={searchTerm}
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
+          </div>
+    </div>
+  );
+}
+
+export default Search;
